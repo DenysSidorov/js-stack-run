4c7a7eb3711e251065edd9873c32cebe24776fff
Denys Sidorov <000scorpions000@gmail.com>
2019-01-18 23:35:16 +0200

    add react test component

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import {a} from './temp';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'My Minimal React Webpack Babel Setup';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
+
 console.log(`A = ${a}`);
