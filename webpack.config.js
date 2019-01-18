ed5406a4778b9688a83e8c42c31737f91a60deb9
Denys Sidorov <000scorpions000@gmail.com>
2019-01-18 23:32:50 +0200

    add babel for new ecmascript

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,19 @@ module.exports = {
     filename: 'bundle.js'
   },
   devServer: {
+    // to serve the files from
     contentBase: './www'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
 };
